5caac6c202856571c786e97eda815ce3e2c13fbf
Alexander Nelson <amvetserv@gmail.com>
2018-06-14 03:32:21 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  SOAD: 'disorder',
+  Coldplay: 'clocks',
+  Loggins: 'danger zone'
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+ 
+  return obj
+}
